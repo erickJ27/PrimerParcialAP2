aa847c9a762a965befb1246ca5880fd0927478a5
Erick J. Gutierrez <50554753+erickJ27@users.noreply.github.com>
2020-02-05 16:32:58 -0300

    subiendo

---
+++ DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerParcialAP2.DAL
+{
+    public class Contexto : DbContext
+    {
+        //public DbSet<Indefinidos> Indefinido { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlServer(@"Server=DESKTOP-SBR4M50\SQLEXPRESS;Database=PrimerPacialDb;Trusted_Connection=true"));
+        }
+    }
+}
